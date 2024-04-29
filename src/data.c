4837e7cd784e649ac38d641f5bb47e6339089d0e
David Ortega Garcia <daortega@car12s3.42barcelona.com>
2024-04-29 16:51:31 +0200

    Daly commit

--- src/data.c
+++ src/data.c
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:49:52 by daortega          #+#    #+#             */
-/*   Updated: 2024/04/16 14:58:49 by daortega         ###   ########.fr       */
+/*   Updated: 2024/04/29 16:48:22 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ void	print_data(t_data data)
 	printf("t_eat: %d\n", data.t_eat);
 	printf("t_sleep: %d\n", data.t_sleep);
 	printf("n_eat: %d\n", data.n_eats);
+}
+
+long long get_t_start(t_data *data)
+{
+	long long result;
+
+	pthread_mutex_lock(&data->getter);
+	result = data->t_start;
+	pthread_mutex_unlock(&data->getter);
+	return(result);	
 }
 
 t_data	fill_data(int argc, char *argv[])
--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,21 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 17:00:50 by daortega          #+#    #+#             */
-/*   Updated: 2024/04/10 14:37:52 by daortega         ###   ########.fr       */
+/*   Updated: 2024/04/29 14:27:08 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libs/philo.h"
 
-int	atoi(char *str)
+long long get_time(void)
+{
+	struct timeval time;
+
+	gettimeofday(&time, NULL);
+	return((long long) time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int	ft_atoi(char *str)
 {
 	int	i;
 	int	result;
